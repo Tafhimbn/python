356a2db206adbde85f560de25a26e219ed9b01b2
Tafhim Bin Nasir <47665581+Tafhimbn@users.noreply.github.com>
2022-12-02 18:20:39 +0600

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,26 @@
+first_name = "Tafhim"
+last_name = "Bin Nasir"
+full_name = first_name +" "+last_name
+id = 123456
+cgpa = 9.88
+
+circuit_marks = 85
+machine_marks = 80
+programming_marks = 91
+managment_marks = 78
+
+total_marks = circuit_marks + machine_marks + programming_marks + managment_marks
+average_marks = total_marks / 4
+
+// Print type of veriables
+print("Name: " + full_name)
+print("ID: " + str(id))
+print("CGPA: "+ str(cgpa))
+print("Total Marks: " + str(total_marks))
+print("Average marks: " + str(average_marks))
+print("")
+print("Name: "+ str(type(full_name)))
+print("ID: "+ str(type(id)))
+print("CGPA: "+ str(type(cgpa)))
+print("Total Marks: "+ str(type(total_marks)))
+print("Average marks: "+ str(type(average_marks)))
