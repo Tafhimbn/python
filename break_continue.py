b0aa5272b950da1cdff3cb6c3a18951e0592fcdb
Tafhimbn <tafhim.aiub@gmail.com>
2024-05-06 22:31:49 +0600

    update

---
+++ break_continue.py
@@ -0,0 +1,47 @@
+# break statement
+
+# break statement with for loop
+
+for letter in 'Tafhim':     
+   if letter == 'h':
+      break
+   print ('Current Letter :', letter)  
+   # print ("Current Letter : {}".format(letter))
+
+
+
+# break statement with while loop 
+i = 11                   
+while i > 0:              
+   print ('Current variable value :', i)
+   i = i -1
+   if i == 5:
+      break
+print ("Stopped !!")
+
+
+
+
+# continue statement
+
+
+# continue statement with for loop
+
+for letter in 'Tafhim':     
+   if letter == 'h':
+      continue
+   print ('Current Letter :', letter)  
+   # print ("Current Letter : {}".format(letter))
+
+
+
+# continue statement with while loop 
+i = 11                   
+while i > 0:              
+   print ('Current variable value :', i)
+   i = i -1
+   if i == 5:
+      continue
+print ("Stopped !!")
+
+
---
+++ pass.py
@@ -0,0 +1,22 @@
+# pass statement
+
+# pass statement with for loop
+for letter in 'Tafhim':     
+   if letter == 'h':
+      pass
+      print ('Pass Letter')  
+   print ('Current Letter :', letter)  
+   # print ("Current Letter : {}".format(letter))
+
+
+
+# pass statement with while loop 
+i = 11                   
+while i > 0:              
+   print ('Current variable value :', i)
+   i = i -1
+   if i == 5:
+      pass
+      print ("passed !!")
+
+
